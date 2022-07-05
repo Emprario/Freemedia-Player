f09a27c93cb754156593b26f4689a0e0a8aed80b
Emprario <95568748+Emprario@users.noreply.github.com>
2022-07-05 18:37:52 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Freemedia-player</title>
+        <!--link rel="icon" type="image/png" sizes="16x16" href="img/ico/icon_x16.png"-->
+		<meta charset="utf-8">	
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet"> 
+        <!--script src = "script/fct.js" type = "text/javascript"></script-->
+        <link rel="stylesheet" type = "text/css" href = "styles.css">
+    </head>
+    <body>
+        <?php
+            $welcome="<p class='user'>Welcome ";
+            $name="Pierre";
+            echo $welcome.$name."!</p>";
+        ?>
+        <p class="titre">Freemedia-Player</p>
+        <p class="newhr"#>________________________________________________________</p>
+    </body>
+</html>
