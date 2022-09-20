b8c80eaafd2fec789d1a0a464f6f1e7f27ec5aac
Emprario <pierre.cameleri@free.fr>
2022-09-20 19:13:38 +0200

    Update to use it on github

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     <head>
         <title>Freemedia-player</title>
         <!--link rel="icon" type="image/png" sizes="16x16" href="img/ico/icon_x16.png"-->
-		<meta charset="utf-8">	
+		<meta charset="unicode">	
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet"> 
@@ -17,5 +17,28 @@
         ?>
         <p class="titre">Freemedia-Player</p>
         <p class="newhr"#>________________________________________________________</p>
+
+        <!--Proceed by boxes-->
+        <table class="main">
+            <tr>
+                <td class="td-box"> <!--Template-->          
+                    <div class="box">
+                        <img src="https://image.tmdb.org/t/p/original/29vpkpY52u8gHAtDK68wv91YgRK.jpg" alt="Titre du Film" class="Preview">
+                        <p class="film-titre">Astérix et le coup du menhir</p>
+                        <div class="description">
+                        <p>
+                        Les garnisons romaines cherchent à tirer profit de l’amnésie soudaine du druide Panoramix, mésestimant la vigilance du vaillant guerrier Astérix.
+                        </p>
+                        
+                        <p class="align">Evaluation global:</p>
+                        <p class="align">★★★</p>
+                        </div>
+                    </div>
+                </td>           
+                <td> <!--Séparation-->
+                    ++++++++++++++<br>++++++++++++++
+                <td>
+            </tr>
+        </table>
     </body>
 </html>
