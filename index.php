0c3d4d9a605a548e4b4ef9a3a00a524a5934e564
Pierre CAMELERI <pierre@TOUR-POWER.localdomain>
2022-09-23 20:13:54 +0200

    Initial setup finished for php future releases

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
     <head>
         <title>Freemedia-player</title>
         <!--link rel="icon" type="image/png" sizes="16x16" href="img/ico/icon_x16.png"-->
-		<meta charset="unicode">	
+		<meta charset="unicode">
+        <meta http-equiv="Cache-control" content="no-cache">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet"> 
@@ -21,23 +22,27 @@
         <!--Proceed by boxes-->
         <table class="main">
             <tr>
-                <td class="td-box"> <!--Template-->          
-                    <div class="box">
-                        <img src="https://image.tmdb.org/t/p/original/29vpkpY52u8gHAtDK68wv91YgRK.jpg" alt="Titre du Film" class="Preview">
-                        <p class="film-titre">Astérix et le coup du menhir</p>
-                        <div class="description">
-                        <p>
-                        Les garnisons romaines cherchent à tirer profit de l’amnésie soudaine du druide Panoramix, mésestimant la vigilance du vaillant guerrier Astérix.
-                        </p>
-                        
-                        <p class="align">Evaluation global:</p>
-                        <p class="align">★★★</p>
-                        </div>
-                    </div>
+                <td> <!--Template-->          
+                    <!--div class="box"-->
+                    <img src="https://image.tmdb.org/t/p/original/29vpkpY52u8gHAtDK68wv91YgRK.jpg" alt="Titre du Film" class="Preview">
+                <td class="td-box">
+                    <p class="film-titre">Astérix et le coup du menhir</p>
+                    <br><br>
+                    <p>Les garnisons romaines cherchent à tirer profit de l’amnésie soudaine du druide Panoramix, mésestimant la vigilance du vaillant guerrier Astérix.</p>
+                    <br>
+                    <p class="align">Evaluation global:</p>
+                    <p class="align">★★★</p>
+                    <br>
+                    <p class="align">Chaine:</p>
+                    <p class="align">W9</p>
+                </td>
+                    <!--/div-->
                 </td>           
                 <td> <!--Séparation-->
-                    ++++++++++++++<br>++++++++++++++
-                <td>
+                    <img src="assets/UP.png" alt="UP arrow">
+                    <br><img src="assets/RED.png" style="height:96px" alt="central point">
+                    <br><img src="assets/DOWN.png" alt="DOWN arrow">
+                </td>
             </tr>
         </table>
     </body>
